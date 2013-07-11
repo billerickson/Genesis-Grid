75d97c029b5ad61e4018dd744b02e34b1f475e65
Jon Brown <jbrown510@gmail.com>
2013-07-10 17:29:12 -1000

    Update plugin.php
    Teaser class, without space.  First and inner page

--- plugin.php
+++ plugin.php
@@ -182,7 +182,8 @@ class BE_Genesis_Grid {
 	
 			// Teasers
 			} else {
-				$classes[] = $teaser_column . 'teaser';
+				$classes[] = $teaser_column;
+				$classes[] = 'teaser';
 				if( 0 == ( $wp_query->current_post - $grid_args['features_on_front'] ) || 0 == ( $wp_query->current_post - $grid_args['features_on_front'] ) % $grid_args['teaser_columns'] )
 					$classes[] = 'first';
 			}
@@ -196,7 +197,8 @@ class BE_Genesis_Grid {
 	
 			// Teasers
 			} else {
-				$classes[] = $teaser_column . 'teaser';
+				$classes[] = $teaser_column;
+				$classes[] = 'teaser';
 				if( 0 == ( $wp_query->current_post - $grid_args['features_inside'] ) || 0 == ( $wp_query->current_post - $grid_args['features_inside'] ) % $grid_args['teaser_columns'] )
 					$classes[] = 'first';
 			}
